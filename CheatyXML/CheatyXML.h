a22260cd9811f2da94f8398608b48aba46041779
Louis Bodart <lo.bodart3@gmail.com>
2015-03-14 18:04:30 +0100

    Initial Commit

---
+++ CheatyXML/CheatyXML.h
@@ -0,0 +1,19 @@
+//
+//  CheatyXML.h
+//  CheatyXML
+//
+//  Created by Louis BODART on 14/03/2015.
+//  Copyright (c) 2015 Louis BODART. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CheatyXML.
+FOUNDATION_EXPORT double CheatyXMLVersionNumber;
+
+//! Project version string for CheatyXML.
+FOUNDATION_EXPORT const unsigned char CheatyXMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheatyXML/PublicHeader.h>
+
+
